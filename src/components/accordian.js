f829dd5676c64c6ab497305b5a5059e7d2a93531
lokeshbhardwaj7 <lokeshbhardwajlj@gmail.com>
2025-09-06 11:54:23 +0530

    Feat: mekanism task

---
+++ src/components/accordian.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/accordian.css";
+import { IoIosArrowForward } from "react-icons/io";
+
+function Accordian() {
+  return (
+    <div className="main-div">
+      Hey
+      <IoIosArrowForward />
+    </div>
+  );
+}
+
+export default Accordian;
